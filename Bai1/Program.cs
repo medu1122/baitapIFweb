569b46e0a83a68d2437066ab634e957f053c7159
medu1122 <79390838+medu1122@users.noreply.github.com>
2023-05-21 14:40:59 +0700

    Add project files.

---
+++ Bai1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace Bai1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Linear Equation Resolver");
+            Console.WriteLine("Given a equation as 'a * x + b = 0', please enter constants:");
+
+            Console.Write(" a = ");
+            double a =double.Parse(Console.ReadLine());
+            Console.Write(" b = ");
+            double b=double.Parse(Console.ReadLine());
+            
+            if (a!=0)
+            {
+                double x = -b / a;
+                Console.WriteLine(" x = " + x);
+            }
+            else if (b==0 )
+            {
+                Console.WriteLine(" vo so nghiem ");
+            }
+            else
+            {
+                Console.WriteLine(" vo nghiem ");
+            }
+        }
+    }
+}
---
+++ Bai2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace Bai2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("hei : ");
+            double hei = double.Parse(Console.ReadLine());
+            Console.Write("wei : ");
+            double wei = double.Parse(Console.ReadLine());
+            double bmi = wei / (hei*hei);
+            if (bmi<18.5)
+            {
+                Console.WriteLine("Underweight ");
+            }
+            else if (bmi<25)
+            {
+                Console.WriteLine("Normal ");
+
+            }
+            else if (bmi<30)
+            {
+                Console.WriteLine("Overweight");
+
+            }
+            else { Console.WriteLine("obese"); }
+        }
+    }
+}
